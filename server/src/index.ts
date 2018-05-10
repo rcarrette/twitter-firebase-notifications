7176ca6ee42c64fac6f74743ebca95a223ce3ddc
rcarrette <romain.carrette@live.fr>
2018-05-10 13:54:31 +0200

    added httpserver and project structure

---
+++ server/src/index.ts
@@ -0,0 +1,5 @@
+import Server from './server'
+
+//server entry point
+new Server(1337)
+    .start()
---
+++ server/src/server.ts
@@ -0,0 +1,41 @@
+import http = require('http')
+
+export default class Server {
+    //#region fields
+
+    private _port: number
+
+    //#endregion
+
+    //region constructor
+
+    constructor(port: number) {
+        this._port = port
+    }
+
+    //#endregion
+
+    //#region methods
+
+    public start(): void {
+        let server = http.createServer(this.onRequestReceived)
+
+        server.listen(this._port, (err) => {
+            if (err)
+                console.log(`twitter-live server failed to start: ${err}`)
+
+            console.log(`twitter-live server running on port ${this._port}`)
+
+            //TODO Twitter client initialization
+        })
+    }
+
+    private onRequestReceived(request, response): void {
+        //TODO proper logging for cloud deployment
+        console.log(`new request received: ${request.url}`)
+
+        response.end()
+    }
+
+    //#endregion
+}
